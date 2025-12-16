f739aec672315fa95c1b0ca1ca9fd3a6c2d00b2d
Stag <email@example.com>
2025-12-15 22:56:45 -1000

    added a header with buttons and the todo class utilizing date-fns

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+import { differenceInDays } from "date-fns";
+
+class todo {
+    constructor (title, description, dueDate, priority, notes) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+    }
+
+    get timeLeft() {
+        const currentDate = new Date()
+        const difference = differenceInDays(
+            this.dueDate,
+            currentDate,
+        )
+        return difference
+    }
+}
+
+
+
+const party = new todo("Birthday Party", "Bob's birthday party", new Date(2025, 11, 25), 1, "")
+console.log(party.timeLeft)
